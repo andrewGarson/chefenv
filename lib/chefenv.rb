8782e53bea5e65b3a4c572479f0d6f4f4ba0a9bd
Andrew Garson <andrew.garson@gmail.com>
2013-03-26 22:02:30 -0400

    move directory definitions in to methods; use ruby methods for symlinking instead of shelling out; get rid of 'current' file and just read the symlink instead

--- lib/chefenv.rb
+++ lib/chefenv.rb
@@ -4,30 +4,27 @@ require "chefenv/version"
 require 'pathname'
 
 module ChefEnv
-  CHEFENV_DIR = File.expand_path("~/.chefenv")
   class Tasks < Thor
 
     desc "list", "list the available chef environments"
     def list
       current = current_environment
       available_environments.each do |env|
-        puts "#{(env == current_environment) ? '*' : ' '} #{env}"
+        puts "#{(env == current) ? '*' : ' '} #{env}"
       end
-      if available_environments.size == 0
+      if available_environments.empty?
         puts "No Environments Available"
       end
     end
 
     desc "use ENV", "use a particular environment"
     def use(env="")
-      unless env == current_environment
-        if available_environments.include?(env)
-          select_environment(env)
-        else
-          puts "#{env} is not a known environment"
-          list
-        end
+      if available_environments.include?(env)
+        select_environment(env)
+      else
+        puts "'#{env}' is not a known environment."
       end
+      list
     end
 
     desc "current", "display the name of the current environment"
@@ -37,10 +34,10 @@ module ChefEnv
 
     desc "init", "initialize the environment"
     def init()
-      FileUtils.mkdir_p(CHEFENV_DIR) unless File.directory(CHEFENV_DIR)
-      FileUtils.mkdir_p(File.join(CHEFENV_DIR, "environments"))
-      current = current_environment
+      FileUtils.mkdir_p(chefenv_dir) unless File.directory?(chefenv_dir)
+      FileUtils.mkdir_p(environments_dir) unless File.directory?(environments_dir)
 
+      current = current_environment
       if current.nil?
         puts 'No environment selected. Picking the first one that I find. You can change it with `chefenv use ENV`'
         available = available_environments
@@ -51,34 +48,53 @@ module ChefEnv
           use(available.first)
         end
         current = current_environment
-        list
-        return
       end
 
       unless available_environments.include?(current)
         puts "'#{current}' is selected as the current environment, but it is not valid. Please select a different environment"
-        `rm -f ~/.chef`
-        `rm -f #{CHEFENV_DIR}/current`
-        list
-        return
+        FileUtils.safe_unlink(chef_dir)
       end
 
+      list
     end
 
     no_tasks do
+
+      def chef_dir
+        File.expand_path("~/.chef")
+      end
+
+      def chefenv_dir
+        File.expand_path("~/.chefenv")
+      end
+
+      def environments_dir
+        File.expand_path('environments', chefenv_dir)
+      end
+
+      def environment_dir(environment)
+        File.expand_path(environment, environments_dir)
+      end
+
       def available_environments
-        Pathname.new(File.expand_path(CHEFENV_DIR)).children.select { |c| c.directory? }.map { |d| d.basename.to_s }
+        Pathname.new(environments_dir).children.select { |c| c.directory? }.map { |d| d.basename.to_s }
+      end
+
+      def environment_available?(env)
+        available_environments.include?(env)
       end
 
       def select_environment(env)
-        File.write(File.expand_path("#{CHEFENV_DIR}/current"), env)
-        `rm -f ~/.chef`
-        `ln -s ~/chef/#{env} ~/.chef`
+        unless current_environment == env
+          File.write(File.expand_path("#{chefenv_dir}/current"), env)
+          FileUtils.safe_unlink(chef_dir)
+          FileUtils.symlink(File.join(environments_dir, env), chef_dir)
+        end
       end
 
       def current_environment
         begin
-          File.read(File.expand_path("~/chef/current")).chomp
+          Pathname.new(File.readlink(File.expand_path('~/.chef'))).split.last.to_s
         rescue Errno::ENOENT
           nil  
         end
